0d76767becc9366d6eacccca7fc447a15f25f416
MaksymPryhorko <78167917+MaksymPryhorko@users.noreply.github.com>
2021-10-19 21:50:17 +0300

    Добавил лоадер.

--- src/Phonebook/Phonebook-selectors.js
+++ src/Phonebook/Phonebook-selectors.js
@@ -1,6 +1,7 @@
 export const getFilter = (state) => state.contacts.filter;
 export const getItems = (state) => state.contacts.items;
 export const getError = (state) => state.contacts.error;
+export const getIsLoading = (state) => state.contacts.isLoading;
 
 export const getVisibleContacts = (state) => {
   const items = getItems(state);
